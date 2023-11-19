a34783b57067d9810f2fd5c808f8782c113a5901
LeeLee-00 <leenoel@live.com>
2023-11-18 21:31:50 -0500

    added more API limit

--- js/dataService.js
+++ js/dataService.js
@@ -2,16 +2,15 @@
 
 
 
-// Fetches the JSON data from the API endpoint and returns the JSON.
 export function fetchData() {
   const API_URL = 'https://data.wa.gov/resource/f6w7-q2d2.json';
-  return fetch(API_URL)
+  const LIMIT = 50000; // Adjust the limit as needed
+
+  return fetch(`${API_URL}?$limit=${LIMIT}`)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
       return response.json();
-      
     });
 }
-
